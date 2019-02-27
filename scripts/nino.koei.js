197cdcfe6c63972ae2256619bab9813bc1be4817
zy410419243 <zy410419243@gmail.com>
2019-02-27 15:02:45 +0800

    build: add dist

---
+++ scripts/nino.koei.js
@@ -0,0 +1,24 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackIncludeAssetsPlugin = require('html-webpack-include-assets-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  externals: {
+    cesium: 'Cesium'
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      {
+        from: 'node_modules/cesium/Build/Cesium',
+        to: 'cesium'
+      }
+    ]),
+    new HtmlWebpackIncludeAssetsPlugin({
+      append: false,
+      assets: ['cesium/Widgets/widgets.css', 'cesium/Cesium.js']
+    }),
+    new webpack.DefinePlugin({
+      CESIUM_BASE_URL: JSON.stringify('cesium')
+    })
+  ]
+};
