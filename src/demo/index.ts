e3266947e869b971d6ee30aa2a3aeeea97d91ad8
zy410419243 <zy410419243@gmail.com>
2019-02-27 15:15:43 +0800

    chore: separate to demo

---
+++ src/demo/index.ts
@@ -0,0 +1,16 @@
+import Trunk from "..";
+
+new Trunk("root", {
+  animation: false,
+  vrButton: false,
+  geocoder: false,
+  infoBox: false,
+  sceneModePicker: false,
+  selectionIndicator: false,
+  timeline: false,
+  navigationHelpButton: false,
+  scene3DOnly: false,
+  homeButton: false,
+  navigationInstructionsInitiallyVisible: false,
+  fullscreenButton: false
+});
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,3 @@
 import Trunk from "./core";
 
-new Trunk("root", {
-  animation: false,
-  // baseLayerPicker: false,
-  vrButton: false,
-  geocoder: false,
-  infoBox: false,
-  sceneModePicker: false,
-  selectionIndicator: false,
-  timeline: false,
-  navigationHelpButton: false,
-  scene3DOnly: false,
-  homeButton: false,
-  navigationInstructionsInitiallyVisible: false,
-  fullscreenButton: false
-});
+export default Trunk;
