53ed101c484c498c632ea8729be9a7ba05bbc9fc
zy410419243 <zy410419243@gmail.com>
2019-03-01 14:27:47 +0800

    pref: update demo for closing modal

--- src/demo/index.ts
+++ src/demo/index.ts
@@ -1,13 +1,38 @@
-import { Trunk } from "..";
-import { ajax } from "../urlHelper";
+import { Trunk } from '..';
+import { ajax } from '../urlHelper';
 
 ajax({
-  url: "../mock/LatLon.json",
+  url: '../mock/LatLon.json',
   success: ({ data }: { data: Array<any> }) => {
-    new Trunk("root", {
+    new Trunk('root', {
       pointDatas: data,
-      modalPaths: ["../mock/3dtileout/tileset.json"],
-      onClick: (id: string) => console.log(id)
+      modalPaths: ['../mock/3dtileout/tileset.json'],
+      onClick: (id: string) => {
+        const iframe: HTMLElement | null = document.getElementById('modal');
+        if (iframe) {
+          iframe.innerHTML = `
+            <iframe
+              src="http://www.google.com?stcd=${id}"
+              style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; z-index: 1;"
+            >
+            </iframe>
+            <div
+              style="position: absolute; top: 10px; right: 10px; z-index: 2;"
+              id="closeButton"
+            >
+              close
+            </div>
+          `;
+          const closeButton: HTMLElement | null = document.getElementById(
+            'closeButton'
+          );
+          if (closeButton) {
+            closeButton.addEventListener('click', _ => {
+              iframe.innerHTML = '';
+            });
+          }
+        }
+      }
     });
   }
 });
