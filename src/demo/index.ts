7e6332a69cb2ac4a36254c2d89dae5084040961a
zy410419243 <zy410419243@gmail.com>
2019-03-12 09:52:04 +0800

    chore: update demo for switch

--- src/demo/index.ts
+++ src/demo/index.ts
@@ -1,77 +1,97 @@
 import { Trunk } from '..';
 import './assets/popup.css';
+import { PointType } from '../core';
 
-const instance = new Trunk('root', {
-  modelPaths: ['../mock/3dtileout/tileset.json'],
-  line: [
-    {
-      dataSource: [
-        {
-          lng: 121.449,
-          lat: 0.0382,
-          height: 28,
-        },
-        {
-          lng: 121.4485,
-          lat: 0.0388,
-          height: 40,
-        },
-        { lng: 121.4485, lat: 0.0406, height: 40 },
-        { lng: 121.449, lat: 0.0392, height: 40 },
-      ],
-      color: 'red',
-      id: 'testLine',
-      width: 5,
-    },
-  ],
-  polygon: [
-    {
-      dataSource: [
-        {
-          lng: 121.449,
-          lat: 0.0382,
-        },
-        {
-          lng: 121.4485,
-          lat: 0.0388,
-        },
-        { lng: 121.4485, lat: 0.0406 },
-        { lng: 121.449, lat: 0.0392 },
-      ],
-      id: 'testId',
-      name: 'testName',
-      color: '#F96',
-      type: 'pin',
-      table: {
-        columns: [
-          { key: 'name', name: '名称' },
-          { key: 'title', name: '标题' },
-          { key: 'place', name: '地点' },
-        ],
-        dataSource: [
-          { name: '名称1', title: '标题1', place: '地点1' },
-          { name: '名称2', title: '标题2', place: '地点2' },
-          { name: '名称3', title: '标题3', place: '地点3' },
-        ],
+const lineProps = [
+  {
+    dataSource: [
+      {
+        lng: 121.449,
+        lat: 0.0382,
+        height: 28,
       },
+      {
+        lng: 121.4485,
+        lat: 0.0388,
+        height: 40,
+      },
+      { lng: 121.4485, lat: 0.0406, height: 40 },
+      { lng: 121.449, lat: 0.0392, height: 40 },
+    ],
+    color: 'red',
+    id: 'testLine',
+    width: 5,
+  },
+];
+const polygonProps = [
+  {
+    dataSource: [
+      {
+        lng: 121.449,
+        lat: 0.0382,
+      },
+      {
+        lng: 121.4485,
+        lat: 0.0388,
+      },
+      { lng: 121.4485, lat: 0.0406 },
+      { lng: 121.449, lat: 0.0392 },
+    ],
+    id: 'testId',
+    name: 'testName',
+    color: '#F96',
+    type: 'pin' as PointType,
+    table: {
+      columns: [
+        { key: 'name', name: '名称' },
+        { key: 'title', name: '标题' },
+        { key: 'place', name: '地点' },
+      ],
+      dataSource: [
+        { name: '名称1', title: '标题1', place: '地点1' },
+        { name: '名称2', title: '标题2', place: '地点2' },
+        { name: '名称3', title: '标题3', place: '地点3' },
+      ],
     },
-  ],
+  },
+];
+
+const trunk = new Trunk('root', {
+  modelPaths: ['../mock/3dtileout/tileset.json'],
+  onMount: (trunk: Trunk) => {
+    const switchWrapper = document.createElement('div');
+    switchWrapper.className = 'switch';
+    switchWrapper.innerText = 'switch';
+    switchWrapper.onclick = () => {
+      trunk.viewer.entities.removeAll();
+      let newPolygonProps = [];
+      for (let item of polygonProps) {
+        newPolygonProps.push({
+          ...item,
+          color: 'rgba(255,255,255,0)',
+        });
+      }
+      trunk.drawPolygon(viewer, newPolygonProps);
+      trunk.drawLine(viewer, lineProps);
+    };
+    document.body.appendChild(switchWrapper);
+  },
   onClick: ({ name: id }: any) => {
     const modal: HTMLElement | null = document.getElementById('modal');
     if (modal) {
       modal.innerHTML = `
-        <div
-          style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; z-index: 1; background: #FFF;"
-        >
-          ${id}
-        </div>
-        <div
-          style="position: absolute; top: 10px; right: 10px; z-index: 2; font-size: 22px; cursor: pointer;"
-          id="closeButton"
-        >
-          X
-        </div>
-      `;
+      <div
+        style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; z-index: 1; background: #FFF;"
+      >
+        ${id}
+      </div>
+      <div
+        style="position: absolute; top: 10px; right: 10px; z-index: 2; font-size: 22px; cursor: pointer;"
+        id="closeButton"
+      >
+        X
+      </div>
+    `;
       const closeButton: HTMLElement | null = document.getElementById(
         'closeButton',
       );
@@ -104,17 +124,19 @@ const instance = new Trunk('root', {
       }
       table += '<div class="popup-tip-container popup-tip"></div>';
       tooltip.innerHTML = `
-        <div style="position: absolute; top: ${y -
-          tableHeight -
-          15}px; left: ${x -
+      <div style="position: absolute; top: ${y -
+        tableHeight -
+        15}px; left: ${x -
         tableWidth / 2}px; width: ${tableWidth}px; height: ${tableHeight}px;">
-          ${table}
-        </div>
-      `;
+        ${table}
+      </div>
+    `;
     } else if (tooltip && !dataItem) {
       tooltip.innerHTML = '';
     }
   },
 });
 
-console.log(instance);
+const { viewer, drawPolygon, drawLine } = trunk;
+drawPolygon(viewer, polygonProps);
+drawLine(viewer, lineProps);
